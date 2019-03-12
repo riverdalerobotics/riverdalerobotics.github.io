1a27123d27fffa92475e52272e656cf8868192d3
GuyBlumenthal <rhindet@gmail.com>
2019-03-12 18:20:33 -0400

    Auto gen final maybe? Have to finish classes and formatting

---
+++ js/gallerypage.js
@@ -0,0 +1,46 @@
+var bCheckEnabled = true;
+var bFinishCheck = false;
+
+var img;
+var imgArray = new Array();
+var i = 1;
+
+var myInterval = setInterval(loadImage, 1);
+
+function loadImage() {
+
+    if (bFinishCheck) {
+        clearInterval(myInterval);
+        imagesToDOM();
+        alert('Loaded ' + i + ' image(s)!)');
+        return;
+    }
+
+    if (bCheckEnabled) {
+
+        bCheckEnabled = false;
+
+        img = new Image();
+        img.onload = fExists;
+        img.onerror = fDoesntExist;
+        img.src = 'images/gallery/gallery-pages/test/image' + i + '.png';
+
+    }
+
+}
+
+function fExists() {
+    imgArray.push(img);
+    i++;
+    bCheckEnabled = true;
+}
+
+function fDoesntExist() {
+    bFinishCheck = true;
+}
+
+function imagesToDOM () {
+  imgArray.forEach(function(element) {
+    document.getElementById("gallery-page").appendChild(element);
+  });
+}

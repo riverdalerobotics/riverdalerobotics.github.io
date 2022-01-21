eb06250a473b020de8364e3ebff51a0f56179006
ck88889 <95720418+ck88889@users.noreply.github.com>
2022-01-21 15:30:59 -0500

    changed  photo gallery back

--- js/gallerypage.js
+++ js/gallerypage.js
@@ -24,11 +24,11 @@ function loadImage() {
         img = new Image();
         img.onload = fExists;
         img.onerror = fDoesntExist;
-        img.src = page + '/image (' + i + ').jpg';
+        img.src = page + '/image (' + i + ').JPG';
         img.className = "gallery-image shadow";
 
         a = document.createElement("a");
-        a.href = page + '/image (' + i + ').jpg';
+        a.href = page + '/image (' + i + ').JPG';
         a.target= "_blank";
         a.appendChild(img);
 
